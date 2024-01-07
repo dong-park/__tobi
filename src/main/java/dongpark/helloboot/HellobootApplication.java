6ad4ca45c4e4d0157d8f47d155533821c835ecc1
dong-park <rakddh@gmail.com>
2024-01-07 19:27:52 +0900

    합성 애노테이션의 적용

--- src/main/java/dongpark/helloboot/HellobootApplication.java
+++ src/main/java/dongpark/helloboot/HellobootApplication.java
@@ -4,24 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan // 이 클래스의 하위 패키지를 스캔해서 @Component가 붙은 클래스를 찾아서 스프링 컨테이너에 등록한다.
+@MySpringBootAnnotation
 public class HellobootApplication {
-
-    @Bean
-    public ServletWebServerFactory serverFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args); // 결국엔 원형으로 돌아갔다..
     }
---
+++ src/main/java/dongpark/helloboot/MySpringBootAnnotation.java
@@ -0,0 +1,15 @@
+package dongpark.helloboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target({java.lang.annotation.ElementType.TYPE})
+@Configuration
+@ComponentScan // 이 클래스의 하위 패키지를 스캔해서 @Component가 붙은 클래스를 찾아서 스프링 컨테이너에 등록한다.
+public @interface MySpringBootAnnotation {
+
+}
