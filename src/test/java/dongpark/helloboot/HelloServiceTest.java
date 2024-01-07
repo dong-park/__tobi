52abba72c4b07ae2b48422c18f1658233eaa1f32
dong-park <rakddh@gmail.com>
2024-01-07 19:20:34 +0900

    메타 에노테이션과 합성 에노테이션 적용해보기

--- src/test/java/dongpark/helloboot/HelloServiceTest.java
+++ src/test/java/dongpark/helloboot/HelloServiceTest.java
@@ -3,15 +3,34 @@ package dongpark.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface FastUnitTest {
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test
+@interface UnitTest {
+
+}
+
+
 public class HelloServiceTest {
-    @Test
+    @FastUnitTest
     void simpleHelloServiceTest() {
         HelloService helloService = new SimpleHelloService();
         String result = helloService.sayHello("dongpark");
         Assertions.assertThat(result).isEqualTo("Hello, dongpark");
     }
 
-    @Test
+    @UnitTest
     void decoratorHelloServiceTest() {
         HelloService helloService = new HelloDecorator(name -> name);
         String result = helloService.sayHello("dongpark");
