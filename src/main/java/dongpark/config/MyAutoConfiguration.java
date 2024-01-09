17255bc4e81b8dfce6b4b622e7753684e5e51e28
dong-park <rakddh@gmail.com>
2024-01-09 11:14:36 +0900

    MyAutoConfiguration 어노테이션 적용 및 proxyBeanMethods 적용

--- src/main/java/dongpark/config/MyAutoConfiguration.java
+++ src/main/java/dongpark/config/MyAutoConfiguration.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target({java.lang.annotation.ElementType.TYPE})
-@Configuration(proxyBeanMethods = false)
+@Configuration(proxyBeanMethods = false) // proxyBeanMethods = false: @Configuration이 붙은 클래스의 @Bean 메소드들은 모두 프록시로 감싸지지 않고 직접 호출된다.
 public @interface MyAutoConfiguration {
 }
--- src/main/java/dongpark/config/autoconfig/TomcatServletWebServerConfig.java
+++ src/main/java/dongpark/config/autoconfig/TomcatServletWebServerConfig.java
@@ -1,10 +1,11 @@
 package dongpark.config.autoconfig;
 
+import dongpark.config.MyAutoConfiguration;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatServletWebServerConfig {
 
     @Bean
