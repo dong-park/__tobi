23a2af1dd336047ba3a68d34c93039448eb06468
dong-park <rakddh@gmail.com>
2024-01-09 12:03:43 +0900

    스타터와 Jetty서버 구성 추가

--- src/main/java/dongpark/config/autoconfig/JettyServletWebServerConfig.java
+++ src/main/java/dongpark/config/autoconfig/JettyServletWebServerConfig.java
@@ -1,16 +1,16 @@
 package dongpark.config.autoconfig;
 
 import dongpark.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatServletWebServerConfig {
+public class JettyServletWebServerConfig {
 
-    @Bean("tomcatServerFactory")
+    @Bean("jettyServerFactory")
     public ServletWebServerFactory serverFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
 }
