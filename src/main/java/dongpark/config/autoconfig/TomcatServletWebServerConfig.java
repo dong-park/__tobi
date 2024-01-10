a8053d910e4806562531556d4a32a12e63b0afd9
dong-park <rakddh@gmail.com>
2024-01-10 13:18:04 +0900

    커스텀 @Conditional

--- src/main/java/dongpark/config/autoconfig/TomcatServletWebServerConfig.java
+++ src/main/java/dongpark/config/autoconfig/TomcatServletWebServerConfig.java
@@ -1,28 +1,17 @@
 package dongpark.config.autoconfig;
 
+import dongpark.config.ConditionalMyOnClass;
 import dongpark.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(TomcatServletWebServerConfig.TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatServletWebServerConfig {
 
     @Bean("tomcatServerFactory")
     public ServletWebServerFactory serverFactory() {
         return new TomcatServletWebServerFactory();
-    }
-
-    static class TomcatCondition implements Condition {
-        // Tomcat을 사용할 수 있는지 여부를 체크하는 로직을 작성한다.
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
-        }
     }
 }
