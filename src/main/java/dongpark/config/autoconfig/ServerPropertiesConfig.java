52cad1284b94c5952ac2aac16b58bffdb0d6bb68
dong-park <rakddh@gmail.com>
2024-01-16 11:36:58 +0900

    프로퍼티 클래스의 분리

---
+++ src/main/java/dongpark/config/autoconfig/ServerPropertiesConfig.java
@@ -0,0 +1,16 @@
+package dongpark.config.autoconfig;
+
+import dongpark.config.MyAutoConfiguration;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@MyAutoConfiguration
+public class ServerPropertiesConfig {
+
+    @Bean
+    public ServerProperties serverProperties(Environment environment) {
+        return Binder.get(environment).bind("", ServerProperties.class).get();
+    }
+
+}
--- src/main/java/dongpark/config/autoconfig/TomcatServletWebServerConfig.java
+++ src/main/java/dongpark/config/autoconfig/TomcatServletWebServerConfig.java
@@ -2,25 +2,22 @@ package dongpark.config.autoconfig;
 
 import dongpark.config.ConditionalMyOnClass;
 import dongpark.config.MyAutoConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatServletWebServerConfig {
 
-    @Value("${contextPath}")
-    String contextPath;
-
-
     @Bean("tomcatServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory serverFactory() {
+    public ServletWebServerFactory serverFactory(ServerProperties serverProperties) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(this.contextPath);
+        factory.setContextPath(serverProperties.getContextPath());
+        factory.setPort(serverProperties.getPort());
         return factory;
     }
 }
