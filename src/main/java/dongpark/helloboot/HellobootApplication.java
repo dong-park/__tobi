159deb6d2c1b332871b17f5c255d74c968380fc0
dong-park <rakddh@gmail.com>
2024-01-23 11:56:32 +0900

    자동구성 조건결과 확인하기

--- src/main/java/dongpark/helloboot/HellobootApplication.java
+++ src/main/java/dongpark/helloboot/HellobootApplication.java
@@ -1,8 +1,11 @@
 package dongpark.helloboot;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
@@ -25,3 +28,33 @@ public class HellobootApplication {
     }
 
 }
+/**
+ * standalone으로 실행하여 자동구성 빈에 대해서 살펴보자
+ */
+//@SpringBootApplication
+//public class SprintBootAutoApplication {
+//
+//    @Bean
+//    ApplicationRunner applicationRunner(ConditionEvaluationReport report) {
+//        return args -> {
+//            System.out.println("ConditionEvaluationReport");
+//            long jmxCount = report.getConditionAndOutcomesBySource().entrySet().stream()
+//                    .filter(co -> co.getValue().isFullMatch())
+//                    .filter(co -> co.getKey().indexOf("Jmx") < 0)
+//                    .map(co -> {
+//                        System.out.println(co.getKey());
+//                        co.getValue().forEach(c ->
+//                                System.out.println("\t" + c.getOutcome()));
+//                        return co;
+//                    }).count();
+//
+//            System.out.println("Jmx Count : " + jmxCount);
+//        };
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SprintBootAutoApplication.class, args);
+//    }
+//
+//}
+
